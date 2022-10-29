72ca982fd3778202b3f542f165962c14937fd183
musawakiliML <musawakiliml@hotmail.com>
2022-10-29 20:57:35 +0100

    fixed app configuration

--- api.py
+++ api.py
@@ -1,17 +1,18 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_cors import CORS
+import os
 
 app = Flask(__name__)
 CORS(app)
 
 @app.get("/api/v1/about")
 def about():
-  return {'slackusername': "Musawakiliml",
+  return jsonify({'slackusername': "Musawakiliml",
             'backend': True,
             'age': 26,
-            'bio': "Hey, there i am an aspiring Machine Learning Engineer. A Python Developer with a zeal to become a grey haired programmer."}
+            'bio': "Hey, there i am an aspiring Machine Learning Engineer. A Python Developer with a zeal to become a grey haired programmer."})
 
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=os.getenv("PORT", default=5000))
