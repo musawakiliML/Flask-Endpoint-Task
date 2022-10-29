00b3ad8ec6aeb7a1fff7163197b516ac39d412ad
musawakiliML <musawakiliml@hotmail.com>
2022-10-29 20:53:30 +0100

    updated with gunicorn and procfile

--- api.py
+++ api.py
@@ -4,7 +4,7 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-@app.get("/")
+@app.get("/api/v1/about")
 def about():
   return {'slackusername': "Musawakiliml",
             'backend': True,
