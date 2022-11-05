2071991def20a03105ecd2e50da90547ee65a0be
musawakiliML <musawakiliml@hotmail.com>
2022-11-05 19:40:38 +0100

    updated

--- api.py
+++ api.py
@@ -45,7 +45,21 @@ class HNGTask2(Resource):
             result = eval(f'{data["x"]}{operation_types[operation_type]}{data["y"]}')
         
         else:
-            pass
+            similar_types = {'sum':'addition', 'difference':'subtraction','product':'multiplication'}
+        
+            response = openai.Completion.create(
+                model="text-davinci-002",
+                prompt=f"{data_operation_type} \n\n| solution | operation_type |",
+                temperature=0,
+                max_tokens=100,
+            )
+            
+        _, result, operation_type, _ = [x.strip() for x in  [y for y in response.choices[0].text.split('\n') if y][-1].split('|')]
+        
+        if not operation_type in similar_types:
+            operation_type = operation_type
+        else:
+            operation_type = similar_types[operation_type]
         
         #response = openai.Completion.create(model="text-davinci-002", prompt="Say this is a test", temperature=0, max_tokens=100)
         
